db643a89e413ba1b3337bee28aff244ff6ed0667
Anna Kapitanova <totoshechka19@gmail.com>
2025-10-23 11:04:57 +0300

    Футер

--- app/components/footer.vue
+++ app/components/footer.vue
@@ -1,11 +1,19 @@
 <template>
-  <footer class="bg-gray-200 text-gray-800 p-4 mt-auto">
-    <div class="container mx-auto">
-      <p>Это компонент Футера</p>
+  <footer class="bg-gray-100 border-t border-gray-200 mt-auto text-sm">
+    <div class="container mx-auto px-4 py-4 flex items-center space-x-6 text-gray-600">
+      <div>
+        &copy; Annonce {{ currentYear }}
+      </div>
+      <nav class="flex items-center space-x-6">
+        <a href="#" class="hover:underline">Документы</a>
+        <a href="#" class="hover:underline">О компании</a>
+      </nav>
     </div>
   </footer>
 </template>
 
-<script setup></script>
+<script setup>
+const currentYear = new Date().getFullYear();
+</script>
 
 <style scoped></style>
